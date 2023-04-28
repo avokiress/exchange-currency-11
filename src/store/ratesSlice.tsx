ef7ea00ac8dd89f81f2fe2fae750961403c06e4b
Kseniya Egzhanova <avokires@yandex.ru>
2023-04-28 16:39:46 +0300

    avokires: create store ratesSlice and fetch data

--- src/store/ratesSlice.tsx
+++ src/store/ratesSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 interface RatesState {
   rates: []
@@ -6,12 +6,32 @@ interface RatesState {
   error: string
 }
 
+interface Returned {
+  rates: []
+}
+
 const initialState = {
   rates: [],
-  loading: false,
+  loading: 'idle',
   error: ''
-} as RatesState;
+} as unknown as RatesState;
 
+const myHeaders = new Headers();
+myHeaders.append("apikey", "Hz7p5ZyXr7AqxDxM9T0Q9ENxMirSqpuX");
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+} as RequestInit;
+
+
+export const fetchRates = createAsyncThunk(
+  'rates/fetchRates',
+  async () => {
+    const response = await fetch('https://api.apilayer.com/exchangerates_data/symbols', requestOptions)
+    return (await response.json()) as Returned;
+  }
+)
 
 const ratesSlice = createSlice({
   name: 'rates',
@@ -22,6 +42,29 @@ const ratesSlice = createSlice({
       console.log('#### getRates action: ', action)
       // state.rates = action.payload.rates;
     }
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchRates.pending, (state, action) => {
+      console.log('#### builder state: ', state)
+      console.log('#### builder action: ', action)
+    })
+
+    builder.addCase(fetchRates.fulfilled, (state, { payload }) => {
+      console.log('#### builder payload: ', payload)
+      console.log('#### builder state: ', state)
+      state.rates.push(...payload);
+    })
+    builder.addCase(fetchRates.rejected, (state, action) => {
+      console.log('#### builder state: ', state)
+      console.log('#### builder action: ', action)
+
+      if (action.payload) {
+        state.error = action.payload.errorMessage
+      } else {
+        state.error = action.error
+      }
+    })
   },
 })
 
