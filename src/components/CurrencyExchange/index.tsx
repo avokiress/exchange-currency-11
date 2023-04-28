9942e97f30b5b21af41241715302c1a5ed52a011
Kseniya Egzhanova <avokires@yandex.ru>
2023-04-29 00:54:11 +0300

    avokires: create dropdown for select currency

--- src/components/CurrencyExchange/index.tsx
+++ src/components/CurrencyExchange/index.tsx
@@ -1,51 +1,33 @@
+import { DropdownCurrency } from 'components/DropdownCurrency'
+import Box from '@mui/material/Box';
+import MultipleStopIcon from '@mui/icons-material/MultipleStop';
 
-import { useState } from 'react';
-import countryData from 'constants/countryData.json';
-import countryCurrencySymbol from 'constants/countryCurrencySymbol';
-
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import FormHelperText from '@mui/material/FormHelperText';
-
-console.log('countryData: ', countryData)
-console.log('countryCurrencySymbol: ', countryCurrencySymbol)
 
 export const CurrencyExchange = () => {
-  const [currency, setCurrency] = useState('');
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setCurrency(event.target.value as string);
-  };
-
-  const renderCurrency = (__currency: string) => {
-    const code = __currency.toLowerCase();
-    const name = countryCurrencySymbol[__currency];
-
-    return (
-      <MenuItem key={code} value={name}>
-        <div style={{ display: 'flex', alignItems: 'center'}}>
-          <div className={`currency-flag currency-flag-xl currency-flag-${code}`}></div>
-          <p>{name}</p>
-        </div>
-      </MenuItem>
-    );
-  };
-
   return (
     <>
       <h2>Currency exchange</h2>
-      <FormHelperText>From</FormHelperText>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box>
+          <DropdownCurrency
+            title="From"
+            field="from"
+            currency="USD"
+          />
+        </Box>
 
-      <Select
-        value={currency}
-        onChange={handleChange}
-        displayEmpty
-      >
-        <MenuItem value="">
-          <em>Select currency</em>
-        </MenuItem>
-        {Object.keys(countryCurrencySymbol).map(currencySymbol => renderCurrency(currencySymbol)) }
-      </Select>
+        <Box>
+          <MultipleStopIcon sx={{ fontSize: 40, margin: '25px 10px 0' }} />
+        </Box>
+
+        <Box>
+          <DropdownCurrency
+            title="To"
+            field="to"
+            currency="EUR"
+          />
+        </Box>
+      </Box>
     </>
   )
 }
---
+++ src/components/DropdownCurrency/index.tsx
@@ -0,0 +1,65 @@
+
+import { useState } from 'react';
+import countryCurrencySymbol from 'constants/countryCurrencySymbol';
+
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import FormHelperText from '@mui/material/FormHelperText';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+
+type DropdownData = {
+  title: string
+  field: string
+  currency: string
+}
+
+export const DropdownCurrency = ({ title, field, currency = '' }: DropdownData) => {
+  const [currencyState, setCurrencyState] = useState(currency);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCurrencyState(event.target.value as string);
+  };
+
+  const renderCurrency = (__currency: string) => {
+    const code = __currency.toLowerCase();
+    const name = countryCurrencySymbol[__currency];
+
+    return (
+      <MenuItem key={__currency} value={__currency}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {__currency &&
+            <>
+              <div className={`currency-flag currency-flag-xl currency-flag-${code}`}></div>
+              <p>{name}</p>
+            </>
+          }
+          {!__currency &&
+            <p>Select currency</p>
+          }
+        </Box>
+      </MenuItem>
+    )
+  };
+
+  return (
+    <>
+      <FormHelperText>{title}</FormHelperText>
+
+      <FormControl sx={{ width: '500px' }}>
+        <Select
+          displayEmpty
+          name={field}
+          value={currencyState}
+          onChange={handleChange}
+          MenuProps={{ PaperProps: { sx: { maxHeight: '400px' } } }}
+          renderValue={() => renderCurrency(currencyState)}
+        >
+
+          {Object.keys(countryCurrencySymbol).map(currencySymbol => renderCurrency(currencySymbol))}
+
+        </Select>
+      </FormControl>
+    </>
+  )
+}
