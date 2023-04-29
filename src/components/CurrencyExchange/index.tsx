035f0d55a13b49cfdeb64100707689b85428d743
Kseniya Egzhanova <avokires@yandex.ru>
2023-04-29 12:34:58 +0300

    avokires: fetch and display data

--- src/components/CurrencyExchange/index.tsx
+++ src/components/CurrencyExchange/index.tsx
@@ -1,29 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import MultipleStopIcon from '@mui/icons-material/MultipleStop';
 
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
+
+import { useFetchConvert } from 'hooks/useFetchConvert'
 import { DropdownCurrency } from 'components/DropdownCurrency'
 import { AmountInput } from 'components/AmountInput'
 
+import countryCurrencySymbol from 'constants/countryCurrencySymbol';
 interface DataHandler {
   name: string
   value: string
 }
 
 interface DataConverter {
-  amount?: string
-  from?: string
-  to?: string
+  amount: string
+  from: string
+  to: string
+}
+
+interface DataFetch {
+  data: object
+  isLoading: boolean
+  error: string
+  fetchData(data: DataConverter): string
 }
 
 
 export const CurrencyExchange = () => {
-  const [dataConverter, setDataConverter] = useState<DataConverter>()
+  const initialData = useRef({
+    amount: '',
+    from: '',
+    to: '',
+  });
+
+  const [dataConverter, setDataConverter] = useState<DataConverter>(initialData.current)
+  const { data, isLoading, error, fetchData } = useFetchConvert<DataFetch>()
+
+  const isValidData = () => {
+    let valid = true;
+    for (const field in dataConverter) {
+      if (dataConverter[field].length === 0) {
+        valid = false;
+      }
+    }
+    return valid;
+  };
 
   useEffect(() => {
-    console.log('dataConverter: ', dataConverter);
+    if (isValidData()) {
+      fetchData(dataConverter);
+    }
   }, [dataConverter])
+
 
   const handleConvert = (data: DataHandler) => {
     setDataConverter(__prevData => ({ ...__prevData, ...data }))
@@ -59,7 +94,30 @@ export const CurrencyExchange = () => {
             onChange={handleConvert}
           />
         </Box>
+        <Box>
+          <Button>
+            <StarIcon fontSize="large" />
+            <StarBorderIcon fontSize="large" />
+          </Button>
+        </Box>
       </Box>
+
+      <Box sx={{ padding: '20px 0' }}>
+        {isLoading &&
+          <CircularProgress />
+        }
+        {!isLoading && data &&
+          <>
+            <h3>Exchange rate for {data.date}</h3>
+            <p>{dataConverter.amount} {countryCurrencySymbol[dataConverter.from]} = {data.result} {dataConverter.to}</p>
+            <p>1 {dataConverter.to} = {data.info.rate} {dataConverter.from}</p>
+          </>
+        }
+        {!isLoading && error &&
+          <Alert severity="error">{error}</Alert>
+        }
+      </Box>
+
     </>
   )
 }
--- src/hooks/useFetchConvert.js
+++ src/hooks/useFetchConvert.js
@@ -1,24 +1,36 @@
 import { useState, useCallback, useEffect } from "react";
 
-export const useFetch = (url, options = {}) => {
+
+const url = 'https://api.apilayer.com/exchangerates_data/convert';
+export const useFetchConvert = () => {
+
   const [isLoading, setLoading] = useState(false);
   const [data, setData] = useState();
   const [error, setError] = useState();
 
-  useEffect(() => {
-    fetchData(url, options);
-  }, []);
+  // useEffect(() => {
+  //   fetchData(options);
+  // }, []);
 
   
-  const fetchData = useCallback((url, options = {}) => {
-    url += '?' + (new URLSearchParams(options)).toString();
+  const fetchData = useCallback((options = {}) => {
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "Hz7p5ZyXr7AqxDxM9T0Q9ENxMirSqpuX");
+
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      headers: myHeaders
+    };
+
+    const newUrl = url + '?' + (new URLSearchParams(options)).toString();
 
     setLoading(true);
-    fetch(url)
+    fetch(newUrl, requestOptions)
       .then((response) => response.json())
       .then((data) => { setData(data); setLoading(false); })
       .catch((error) => { setError(error); setLoading(false); });
   });
 
-  return { data, isLoading, error, refetch: fetchData };
+  return { data, isLoading, error, fetchData };
 };
