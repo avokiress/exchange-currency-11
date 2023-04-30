8261a24bf24d96b0c035910ccd1082ce68d53080
Kseniya Egzhanova <avokires@yandex.ru>
2023-04-30 19:24:21 +0300

    avokires: refactoring

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,26 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { fetchSymbols } from 'store/symbolsSlice';
 import { CurrencyExchange } from 'components/CurrencyExchange'
-import { Chart } from "components/Chart"
 import ThemeButton from "../../components/Theme/ThemeButton.jsx";
 import ModalSelectRegion from "../ModalSelectRegion";
 
 export const Main = () => {
-  const dispatch = useDispatch()
-  const { symbols, loading } = useSelector((state) => state.symbols)
-
-  useEffect(() => {
-    // dispatch(fetchSymbols())
-  }, [])
-  // const theme = useTheme()
-  // const colorMode = React.useContext(ColorModeContext);
-  
-  if (loading) return <p>Loading...</p>
-
-
-
   return (
     <>
     <ThemeButton />
